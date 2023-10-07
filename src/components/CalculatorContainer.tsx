dff0997a42bbdef5fbd401d4d4f44bd52fb94760
Sitthata <aprils96950@gmail.com>
2023-10-07 19:04:39 +0700

    Update state management in Calculator Container

--- src/components/CalculatorContainer.tsx
+++ src/components/CalculatorContainer.tsx
@@ -3,23 +3,114 @@ import CalculatorHeader from "./CalculatorHeader";
 import Display from "./Display";
 import KeyPads from "./KeyPads";
 
+type CalculatorState = {
+  currentValue: string;
+  previousValue: string;
+  operator: string | null;
+};
+
 const CalculatorContainer = () => {
-  const [display, setDisplay] = useState<string | undefined>(undefined);
-  const handleClick = (value: string) => {
-    if (value === "DEL") {
-      if (!display) return;
-      if (display.length === 1) return setDisplay(undefined);
-      setDisplay(display.slice(0, -1));
-      return;
+  const initialState: CalculatorState = {
+    currentValue: "0",
+    previousValue: "0",
+    operator: null,
+  };
+  const [calculatorState, setCalculatorState] =
+    useState<CalculatorState>(initialState);
+  // TODO:
+  const handleNumber = (value: string) => {
+    if (calculatorState.currentValue === "0") {
+      setCalculatorState({
+        ...calculatorState,
+        currentValue: value,
+      });
+    } else {
+      setCalculatorState({
+        ...calculatorState,
+        currentValue: calculatorState.currentValue + value,
+      });
     }
-    if (!display) return setDisplay(value);
-    setDisplay(display + value);
+  };
+  // TODO:
+  const handleOperator = (value: string) => {
+    setCalculatorState({
+      ...calculatorState,
+      previousValue: calculatorState.currentValue,
+      currentValue: "0",
+      operator: value,
+    });
+  };
+
+  // TODO:
+  const handleEqual = () => {
+    if (calculatorState.operator && calculatorState.previousValue) {
+      let result = 0;
+      switch (calculatorState.operator) {
+        case "+":
+          result =
+            parseFloat(calculatorState.previousValue) +
+            parseFloat(calculatorState.currentValue);
+          break;
+        case "-":
+          result =
+            parseFloat(calculatorState.previousValue) -
+            parseFloat(calculatorState.currentValue);
+          break;
+        case "x":
+          result =
+            parseFloat(calculatorState.previousValue) *
+            parseFloat(calculatorState.currentValue);
+          break;
+        case "/":
+          if (calculatorState.currentValue === "0") return;
+          result =
+            parseFloat(calculatorState.previousValue) /
+            parseFloat(calculatorState.currentValue);
+          break;
+        default:
+          break;
+      }
+      setCalculatorState({
+        currentValue: result.toString(),
+        previousValue: "",
+        operator: null,
+      });
+    }
+  };
+
+  const handleClick = (value: string) => {
+    if (!isNaN(parseInt(value))) {
+      handleNumber(value);
+    } else {
+      switch (value) {
+        // Reset
+        case "C":
+          setCalculatorState(initialState);
+          break;
+        // Delete
+        case "DEL":
+          setCalculatorState({
+            ...calculatorState,
+            currentValue: calculatorState.currentValue.slice(0, -1) || "0",
+          });
+          break;
+        // Operator
+        case "+":
+        case "-":
+        case "x":
+        case "/":
+          handleOperator(value);
+          break;
+        case "=":
+          handleEqual();
+      }
+    }
   };
   return (
     <div className="flex flex-col items-center mt-[2rem] outline outline-1">
       <div className="min-w-[30rem]">
         <CalculatorHeader />
-        <Display display={display} setDisplay={setDisplay}/>
+        <Display display={calculatorState} />
         <KeyPads onClick={handleClick} />
       </div>
     </div>
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,13 +1,18 @@
-
 type DisplayProps = {
-  display?: string;
-  setDisplay: (value: string | undefined) => void;
+  display: {
+    currentValue: string;
+    previousValue: string;
+    operator: string | null;
+  };
 };
 
-const Display = ({ display = "0" }: DisplayProps) => {
+const Display = ({ display }: DisplayProps) => {
   return (
     <div className="bg-dark-main-bg-300 p-[1rem] py-5 rounded-lg text-right">
-      <h1 className="text-4xl text-neutral-100">{display}</h1>
+      <h1 className="text-4xl text-neutral-100">{display.currentValue}</h1>
+      <p className="text-xl text-neutral-100">
+        {display.previousValue === "0" ? " " : display.previousValue}
+      </p>
     </div>
   );
 };
